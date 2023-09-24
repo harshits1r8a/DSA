0cdd83707533b47d586f09306ddb1a66eeab7583
harshits1r8a <harshsharma.h8a1r3@gmail.com>
2023-09-24 07:43:17 +0530

    BS little-bit change in LC33

--- Leetcode/LC33.java
+++ Leetcode/LC33.java
@@ -2,8 +2,8 @@ package Leetcode;
 
 public class LC33 {
     public static void main(String[] args) {
-        int[] nums = {4,5,6,7,0,1,2};
-        int target = 3;
+        int[] nums = {5,1,3};
+        int target = 5;
         int maxInd = pivot(nums);
         int result = BS(nums,target,maxInd+1,nums.length-1);
         if(result == -1){
@@ -13,11 +13,14 @@ public class LC33 {
     }
 
     static int pivot(int[] arr){
+        if(arr.length ==1){
+            return 0;
+        }
         int s = 0;
         int e = arr.length-1;
-        while(s < e){
+        while(s <= e){
             int mid = s+(e-s)/2;
-            if(arr[mid] > arr[mid-1] && arr[mid] < arr[mid+1]){
+            if(arr[mid] < arr[mid+1]){
                 if(arr[mid] > arr[arr.length-1]){
                     s=mid+1;
                 }else{
@@ -25,7 +28,7 @@ public class LC33 {
                 }
             }else if(arr[mid] < arr[mid+1] && arr[mid] <arr[mid-1]){
                 return mid-1;
-            }else if(arr[mid] > arr[mid+1] && arr[mid] >arr[mid-1]){
+            }else{
                 return mid;
             }
 
