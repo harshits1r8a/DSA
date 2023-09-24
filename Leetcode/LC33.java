ef8b44fff5cba83f3355d508704c3c13d63ed30f
harshits1r8a <harshsharma.h8a1r3@gmail.com>
2023-09-24 07:26:37 +0530

    BS leetcode1095

---
+++ Leetcode/LC33.java
@@ -0,0 +1,50 @@
+package Leetcode;
+
+public class LC33 {
+    public static void main(String[] args) {
+        int[] nums = {4,5,6,7,0,1,2};
+        int target = 3;
+        int maxInd = pivot(nums);
+        int result = BS(nums,target,maxInd+1,nums.length-1);
+        if(result == -1){
+            result =  BS(nums,target,0,maxInd);
+        }
+        System.out.println(result);
+    }
+
+    static int pivot(int[] arr){
+        int s = 0;
+        int e = arr.length-1;
+        while(s < e){
+            int mid = s+(e-s)/2;
+            if(arr[mid] > arr[mid-1] && arr[mid] < arr[mid+1]){
+                if(arr[mid] > arr[arr.length-1]){
+                    s=mid+1;
+                }else{
+                    e=mid-1;
+                }
+            }else if(arr[mid] < arr[mid+1] && arr[mid] <arr[mid-1]){
+                return mid-1;
+            }else if(arr[mid] > arr[mid+1] && arr[mid] >arr[mid-1]){
+                return mid;
+            }
+
+        }
+        return -1;
+    }
+
+
+    static int BS(int[] arr, int target,int s,int e ){
+        while(s<=e){
+            int mid = s+(e-s)/2;
+            if(arr[mid]>target){
+                e = mid-1;
+            }else if(arr[mid]<target){
+                s=mid+1;
+            }else{
+                return mid;
+            }
+        }
+        return -1;
+    }
+}
