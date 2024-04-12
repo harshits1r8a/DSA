1744c00558439cbe444c6195c819cd8f513b1d2f
harshits1r8a <harshsharma.h8a1r3@gmail.com>
2024-04-13 00:25:46 +0530

    cnt rotation gfg

--- BinarySearch/RotationCnt.java
+++ BinarySearch/RotationCnt.java
@@ -1,51 +1,19 @@
 package BinarySearch;
 
-public class SearchinRoteatedArr {
+public class RotationCnt {
     public static void main(String[] args) {
-        int[] arr = {2,2,2,9,2,2};
-        System.out.println(findPivotWithDuplicates(arr));
+        int[] arr = {4, 5, 6, 7, 0, 1, 2};
+        System.out.println(countRotations(arr));
     }
 
-    static int search(int[] nums, int target) {
-        int pivot = findPivot(nums);
-
-        // if you did not find a pivot, it means the array is not rotated
-        if (pivot == -1) {
-            // just do normal binary search
-            return binarySearch(nums, target, 0 , nums.length - 1);
+    private static int countRotations(int[] arr) {
+        int pivot = findPivot(arr);
+        if(pivot == -1){
+            return 0;
         }
-
-        // if pivot is found, you have found 2 asc sorted arrays
-        if (nums[pivot] == target) {
-            return pivot;
-        }
-
-        if (target >= nums[0]) {
-            return binarySearch(nums, target, 0, pivot - 1);
-        }
-
-        return binarySearch(nums, target, pivot + 1, nums.length - 1);
+        return pivot+1;
     }
 
-    static int binarySearch(int[] arr, int target, int start, int end) {
-        while(start <= end) {
-            // find the middle element
-//            int mid = (start + end) / 2; // might be possible that (start + end) exceeds the range of int in java
-            int mid = start + (end - start) / 2;
-
-            if (target < arr[mid]) {
-                end = mid - 1;
-            } else if (target > arr[mid]) {
-                start = mid + 1;
-            } else {
-                // ans found
-                return mid;
-            }
-        }
-        return -1;
-    }
-
-    // this will not work in duplicate values
     static int findPivot(int[] arr) {
         int start = 0;
         int end = arr.length - 1;
@@ -105,5 +73,4 @@ public class SearchinRoteatedArr {
         }
         return -1;
     }
-
 }
